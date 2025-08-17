2cf756ca7bb72810c06b4c3b77a68f61fd7c1586
Isaac Bueno <ibueno.dev@gmail.com>
2025-08-17 18:02:04 -0300

    Integrando com mysql e configurado para acesso a base fluorita-dev .

--- fluorita/settings.py
+++ fluorita/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'fluorita.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fluorita-dev',          # O nome do banco que você criou no Passo 1
+        'USER': 'fluorita',  # <-- SUBSTITUA pelo seu usuário do MariaDB (ex: 'root')
+        'PASSWORD': 'A1s2d3@@', # <-- SUBSTITUA pela sua senha
+        'HOST': '127.0.0.1',            # 'localhost' também funciona
+        'PORT': '3306',                 # Porta padrão do MariaDB/MySQL
     }
 }
 
