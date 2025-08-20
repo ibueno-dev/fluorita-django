3bebe40979c48c8eb03ec76f73a668d3e6976295
Isaac Bueno <ibueno.dev@gmail.com>
2025-08-20 16:39:18 -0300

    verifica se o projeto está em modo de depuração (DEBUG = True) e, em caso afirmativo, adiciona uma rota de URL para servir os arquivos que estão na sua pasta media

--- fluorita/urls.py
+++ fluorita/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('', include('loja.urls', namespace='loja')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
